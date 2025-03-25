24c336622a7eb221a98f5f1cdca77e32c1114241
PieroMeza10 <pieroms0905@gmail.com>
2025-03-25 16:48:13 -0500

    Principal 1

---
+++ src/Genericidad1/Principal.java
@@ -0,0 +1,13 @@
+package Genericidad1;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+    
+        Bolsa < Chocolatina > bolsaCho = new Bolsa < Chocolatina > ();
+    
+        Chocolatina c = new Chocolatina("milka");
+    
+        Chocolatina c1 = new Chocolatina("milka");
+    
+        Chocolatina c2 = new Chocolatina("ferrero"); 
