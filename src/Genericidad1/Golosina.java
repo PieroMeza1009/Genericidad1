bd24c72ed52d1a5a73197e7c8fdfec95937e9575
PieroMeza10 <pieroms0905@gmail.com>
2025-03-25 16:43:59 -0500

    Golosina 1

---
+++ src/Genericidad1/Golosina.java
@@ -0,0 +1,15 @@
+package Genericidad1;
+
+public class Golosina {
+  
+    private String nombre;
+    
+    private double peso;
+    
+    public Golosina(String nombre, double peso) {
+    
+        this.nombre = nombre;
+    
+        this.peso = peso;
+    }
+}
