aef455b7ec77f9bc996dec0d3b37db17a8956c0d
PieroMeza10 <pieroms0905@gmail.com>
2025-03-27 14:41:30 -0500

    bolsa3 o 4

--- src/Genericidad1/Bolsa.java
+++ src/Genericidad1/Bolsa.java
@@ -2,10 +2,12 @@ package Genericidad1;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+//Defino la clase generica Bolsa que implementa Iterable
 public class Bolsa < T > implements Iterable < T > {
-    private ArrayList < T > lista = new ArrayList < T > ();
-    private int tope;
+    private ArrayList < T > lista = new ArrayList < T > ();// Lista interna donde almacenamos los objetos
+    private int tope; // aca esta el lmite de elementos que puede contener la bolsa
 
+    // constructor que inicializa el tope de la bolsa
     public Bolsa(int tope) {    
 
         super();
@@ -13,15 +15,17 @@ public class Bolsa < T > implements Iterable < T > {
         this.tope = tope;
 
     }
+    // metodo para agregar un objeto a la bolsa
     public void add(T objeto) {
-        if (lista.size()>= tope){
-            lista.add(objeto);
 
+        //size es crucial para asegurarse de que no se agreguen más elementos, pues cuenta cuantos elementos hay en la lista
+        if (lista.size() >= tope) { // si la bolsa ha alcanzado su límite
+            throw new RuntimeException("No caben más"); //lanzamos una excepción
         } else {
-            throw new RuntimeException("No caben mas");
-
+            lista.add(objeto); // Agregamos el objeto a la lista
         }
-    }
+    }   
+    // metodo iterator para recorrer la bolsa con un iterador
     public Iterator <T> iterator(){
         return lista.iterator();
     }
