29947d6c40fc3263f341b06c58168e4183b6916c
PieroMeza10 <pieroms0905@gmail.com>
2025-03-27 14:44:47 -0500

    chocolatinna 3

--- src/Genericidad1/Chocolatina.java
+++ src/Genericidad1/Chocolatina.java
@@ -1,17 +1,18 @@
 package Genericidad1;
-
+//defino la clase Chocolatina
 public class Chocolatina {
 
-    private  String marca;
+    private  String marca;//atributo donde se almacena la marca de la chocolate
 
+    ///constructor que recibir la marca de la chocolatina
     public Chocolatina (String marca){
         this.marca = marca;
     }
     
-    public String getMarca() {
+    public String getMarca() {  // /metodo para obtener la marca
         return marca;
     }
-    public void setMarca(String marca){
+    public void setMarca(String marca){ //ste para modificar la marca
         this.marca = marca;
     }
 
