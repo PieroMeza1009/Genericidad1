9ec5f7c242cdc1f1708a04a85f3e3f37609de92c
PieroMeza10 <pieroms0905@gmail.com>
2025-03-27 20:48:26 -0500

    Cajoneria 2

--- src/Genericidad1/Cajoneria.java
+++ src/Genericidad1/Cajoneria.java
@@ -1,10 +1,21 @@
 package Genericidad1;
 
+import java.util.ArrayList;
+
 class Cajoneria<T> implements Iterable<T> {
     private ArrayList<T> lista = new ArrayList<T>();
     private int tope;
 
     public Cajoneria(int tope) {
         this.tope = tope;
+    }
+
+
+    public void add(T objeto) {
+        if (lista.size() >= tope) {
+            throw new RuntimeException("No caben más");
+        } else {
+            lista.add(objeto);
+        }
     }
 }
