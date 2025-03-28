f3e3818c040591d9f5d519a890ada507d0539a3b
PieroMeza10 <pieroms0905@gmail.com>
2025-03-27 20:32:03 -0500

    Caja 1

---
+++ src/Genericidad1/Caja.java
@@ -0,0 +1,14 @@
+package Genericidad1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+// Clase Caja
+public class Caja {
+    private String color;
+    private Object contenido;
+
+    public Caja(String color) {
+        this.color = color;
+    }
+}
