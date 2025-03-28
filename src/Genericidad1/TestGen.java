f07da78da4d7d1c814793b305ddb32e2f8d2820a
PieroMeza10 <pieroms0905@gmail.com>
2025-03-27 20:18:05 -0500

    TG 2

--- src/Genericidad1/TestGen.java
+++ src/Genericidad1/TestGen.java
@@ -9,4 +9,13 @@ public class TestGen {
         }
         return false;
     }
+    public static void main(String[] args) {
+        String[] v = {"Perez", "Sanchez", "Rodriguez"};
+        Integer[] w = {12, 34, 56};
+
+        System.out.println(exist(v, "Sanchez")); // true
+        System.out.println(exist(w, 34)); // true
+        System.out.println(exist(w, 100)); // false
+    }
 }
+
