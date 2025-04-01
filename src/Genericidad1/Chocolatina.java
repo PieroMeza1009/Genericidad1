d21cb2e62790a9497a0027eed06caa31f9abf4c1
PieroMeza10 <pieroms0905@gmail.com>
2025-04-01 15:45:54 -0500

    chocolatina 10.2

--- src/Genericidad1/Chocolatina.java
+++ src/Genericidad1/Chocolatina.java
@@ -6,7 +6,7 @@ public class Chocolatina {
 
     ///constructor que recibir la marca de la chocolatina
     public Chocolatina (String marca){
-        this.marca = marca;
+        this.marca = marca; //Se usa "this" para diferenciar el atributo del parámetro
     }
     
     public String getMarca() {  // /metodo para obtener la marca
@@ -16,15 +16,16 @@ public class Chocolatina {
         this.marca = marca;
     }
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(Object obj) {   //Método equals  Sirve para comparar si dos objetos Chocolatina son iguales
         if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Chocolatina that = (Chocolatina) obj;
-        return marca.equals(that.marca);
+        if (obj == null || getClass() != obj.getClass()) return false; // getclass para obtener la clase en tiempo de ejecución de un objeto
+        Chocolatina that = (Chocolatina) obj;   //Se convierte obj a Chocolatina, ya que sabemos que son del mismo tipo tras la validación anterior
+        return marca.equals(that.marca);   // Compara las marcas de ambas chocolatinas
     }
 
     @Override
-    public String toString() {
+    public String toString() {  //Devuelve una representación en texto del objeto Chocolatina
+
         return "Chocolatina{" + "marca='" + marca + "'}";
     }
 }
